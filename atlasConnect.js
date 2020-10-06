4fc13f38b3679966b5e77ae301963441dad48401
DylanFurner-71 <jdfurner@gmail.com>
2020-10-06 18:25:57 -0500

    MongoDB working successfully, FE successfully merged

--- atlasConnect.js
+++ atlasConnect.js
@@ -1,23 +1,18 @@
-import { default as mongodb } from "mongodb";
-const MongoClient = mongodb.MongoClient;
+import mongoose from "mongoose";
 
 export const dbName = "testHaircutBE"; //the name of our database
 /* this is the db we will work with */
 //export const ATLAS_URL = `mongodb+srv://dfurner:<smu2020>@cluster0.tkm1w.azure.mongodb.net/<${dbName}>?retryWrites=true&w=majority`; //We are configuring urls here
 
-export const ATLAS_URL = `mongodb+srv://elisego45:120060@segoviacluster0.zz1fg.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+export const ATLAS_URL = `mongodb+srv://dfurner:smu2020@cluster0.tkm1w.azure.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-const client = new MongoClient(ATLAS_URL);
-export default class atlassConnect {}
+export const mongooseConnect = async () => {
+await mongoose.connect(ATLAS_URL, { useUnifiedTopology: true, useCreateIndex: true })
+.then(() => {
 
-export async function connect() {
-  console.log("we are here234234234234");
-  try {
-    await client.connect();
-    console.log("Connected correctly to server");
-  } catch (err) {
-    console.log(err.stack);
-  } finally {
-    await client.close();
-  }
-}
+    console.log("MongoDB database connection established successfully");
+  });
+};
+
+module.exports = mongooseConnect;
+
--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import routes from "./routes/index.js";
-
+const mongooseConnect = require('./atlasConnect');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+mongooseConnect();
 
-const uri =
-  "mongodb+srv://elisego45:120060@segoviacluster0.zz1fg.mongodb.net/testHaircutBE?retryWrites=true&w=majority";
-mongoose.connect(uri, { useUnifiedTopology: true, useCreateIndex: true });
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
 //routes to our app
 app.get("/", (req, res) => {
   res.send("/ is running just fine");
