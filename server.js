e8383db1c2191792a8c0ee6feb693b03e37ff5ae
Eli <elisego45@gmail.com>
2020-10-11 13:15:22 -0500

    update

--- server.js
+++ server.js
@@ -3,16 +3,8 @@ dotenv.config({ path: "./config/config.env" });
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
-<<<<<<< HEAD
 import { mongooseConnect } from "./atlasConnect.js";
-
 const app = express();
-=======
-const mongooseConnect = require('./atlasConnect');
-const app = express();
-const port = 8000;
->>>>>>> 6477959719d243c526def3ec0c07d550b4bb806c
-
 app.use(cors());
 app.use(express.json());
 mongooseConnect();
