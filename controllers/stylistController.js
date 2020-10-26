6d74d3c2c468077c8ade60b5dd70d08fb34c2a43
Eli <elisego45@gmail.com>
2020-10-26 13:51:40 -0500

    Add get logged in user route

--- controllers/stylistController.js
+++ controllers/stylistController.js
@@ -1,93 +1,97 @@
-import mongoose from "mongoose";
-import Stylist from "../models/stylistModel.js";
-import ErrorResponse from "../utils/errorResponse.js";
+import mongoose from 'mongoose';
+import Stylist from '../models/stylistModel.js';
+import ErrorResponse from '../utils/errorResponse.js';
 
 //@desc          Get all stylists from DB
 //@route         GET /stylists
 //@access        Private?
 export const getStylists = async (req, res, next) => {
-  try {
-    const stylist = await Stylist.find();
-    res.json(stylist);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const stylist = await Stylist.find();
+        res.json(stylist);
+    } catch (err) {
+        next(err);
+    }
 };
 
 //@desc          Login in stylist
 //@route         POST /stylists/login
 //@access        Public
 export const stylistLogin = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (!email || !password) {
-    return next(new ErrorResponse("Please provide an email and password", 400));
-  }
-  try {
-    const currStylist = await Stylist.findOne({ email }).select("+password");
-    if (!currStylist) {
-      return next(new ErrorResponse("Invalid Credentials", 401));
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !password) {
+        return next(
+            new ErrorResponse('Please provide an email and password', 400)
+        );
     }
-    const isMatch = await currStylist.matchPassword(password);
-    if (!isMatch) {
-      return next(new ErrorResponse("Invalid Credentials", 401));
+    try {
+        const currStylist = await Stylist.findOne({ email }).select(
+            '+password'
+        );
+        if (!currStylist) {
+            return next(new ErrorResponse('Invalid Credentials', 401));
+        }
+        const isMatch = await currStylist.matchPassword(password);
+        if (!isMatch) {
+            return next(new ErrorResponse('Invalid Credentials', 401));
+        }
+        const token = currStylist.getSignedJwtToken();
+        currStylist.lastLogin = Date.now();
+        currStylist.save();
+        res.status(200).json({ success: true, token, stylist: currStylist });
+    } catch (err) {
+        next(err);
     }
-    const token = currStylist.getSignedJwtToken();
-    currStylist.lastLogin = Date.now();
-    currStylist.save();
-    res.status(200).json({ success: true, token, stylist: currStylist });
-  } catch (err) {
-    next(err);
-  }
 };
 
 //@desc          Register a new stylist account
 //@route         POST /stylists/register
 //@access        Public
 export const createStylist = async (req, res, next) => {
-  const stylist = new Stylist(req.body);
-  try {
-    const newStylist = await stylist.save();
-    //create token
-    const token = stylist.getSignedJwtToken();
+    const stylist = new Stylist(req.body);
+    try {
+        const newStylist = await stylist.save();
+        //create token
+        const token = stylist.getSignedJwtToken();
 
-    res.json({ sucess: true, token, newStylist });
-  } catch (err) {
-    next(err);
-  }
+        res.json({ sucess: true, token, newStylist });
+    } catch (err) {
+        next(err);
+    }
 };
 
 export const updateStylist = async (req, res, next) => {
-  try {
-    const stylist = await Stylist.findById(req.params.id);
+    try {
+        const stylist = await Stylist.findById(req.params.id);
 
-    if (!stylist) {
-      return next(new ErrorResponse("Cannot Find Resource", 404));
+        if (!stylist) {
+            return next(new ErrorResponse('Cannot Find Resource', 404));
+        }
+        if (!req.stylist || req.stylist.id !== stylist.id) {
+            return next(new ErrorResponse('Unauthorized', 401));
+        }
+        [
+            'address',
+            'firstName',
+            'lastName',
+            'photo',
+            'businessName',
+            'number',
+            'services',
+        ].forEach(prop => {
+            if (req.body[prop] && req.body[prop] !== stylist[prop]) {
+                stylist[prop] = req.body[prop];
+            }
+        });
+        await stylist.save();
+        res.status(200).json({
+            sucess: true,
+            stylist,
+        });
+    } catch (err) {
+        return next(new ErrorResponse(err));
     }
-    if (!req.user || req.user.id !== stylist.id) {
-      return next(new ErrorResponse("Unauthorized", 401));
-    }
-    [
-      "address",
-      "firstName",
-      "lastName",
-      "photo",
-      "businessName",
-      "number",
-      "services",
-    ].forEach((prop) => {
-      if (req.body[prop] && req.body[prop] !== stylist[prop]) {
-        stylist[prop] = req.body[prop];
-      }
-    });
-    await stylist.save();
-    res.status(200).json({
-      sucess: true,
-      stylist,
-    });
-  } catch (err) {
-    return next(new ErrorResponse(err));
-  }
 };
 
 //@desc          Change Stylist Password
@@ -95,29 +99,45 @@ export const updateStylist = async (req, res, next) => {
 //@access        Private
 // needs to change!!!
 export const changePassword = async (req, res, next) => {
-  try {
-    const currStylist = await Stylist.findById(req.params.stylistId).select(
-      "+password"
-    );
-    if (!req.user || req.user.id !== currStylist.id) {
-      return next(new ErrorResponse("Unauthorized", 401));
+    try {
+        const currStylist = await Stylist.findById(req.params.stylistId).select(
+            '+password'
+        );
+        if (!req.stylist || req.stylist.id !== currStylist.id) {
+            return next(new ErrorResponse('Unauthorized', 401));
+        }
+        const isMatch = await currStylist.matchPassword(req.body.password);
+        if (!isMatch) {
+            return next(new ErrorResponse('Invalid password', 400));
+        }
+        if (req.body.newPassword !== req.body.newPasswordConf) {
+            return next(new ErrorResponse('new passwords do not match', 400));
+        }
+        currStylist.password = req.body.newPassword;
+        await currStylist.save();
+        res.status(200).json({
+            success: true,
+            msg: 'Password Updated',
+        });
+    } catch (err) {
+        next(err);
     }
-    const isMatch = await currStylist.matchPassword(req.body.password);
-    if (!isMatch) {
-      return next(new ErrorResponse("Invalid password", 400));
+};
+
+//@desc          Get current logged in stylist
+//@route         GET /stylists/me
+//@access        Private
+
+export const getMe = async (req, res, next) => {
+    try {
+        const stylist = await Stylist.findById(req.stylist.id);
+        res.status(200).json({
+            success: true,
+            stylist,
+        });
+    } catch (err) {
+        next(err);
     }
-    if (req.body.newPassword !== req.body.newPasswordConf) {
-      return next(new ErrorResponse("new passwords do not match", 400));
-    }
-    currStylist.password = req.body.newPassword;
-    await currStylist.save();
-    res.status(200).json({
-      success: true,
-      msg: "Password Updated",
-    });
-  } catch (err) {
-    next(err);
-  }
 };
 
 //@desc          Search STylist by name
@@ -132,45 +152,48 @@ export const changePassword = async (req, res, next) => {
  * services=<services desired - space-seperated string list>
  */
 export const searchStylist = async (req, res) => {
-  try {
-    const { name, within, min, rat, services } = req.query;
-    let returnedStylists;
+    try {
+        const { name, within, min, rat, services } = req.query;
+        let returnedStylists;
 
-    // search by name logic
-    //-------------------------------------------------------
-    if (name) {
-      let names = name.split(" ");
-      for (let key in names) {
-        names[key] = new RegExp(names[key], "i");
-      }
-      //finds the stylists based on search query
-      returnedStylists = await Stylist.find({
-        $or: [{ firstName: { $in: names } }, { lastName: { $in: names } }],
-      });
-    } else {
-      returnedStylists = await Stylist.find();
-    }
-
-    //search by services logic
-    //-------------------------------------------------------
-    if (services) {
-      returnedStylists = returnedStylists.filter((stylist) => {
-        let hasServices = true;
-        for (let service of services.split(" ")) {
-          if (stylist.services.indexOf(service) === -1) {
-            hasServices = false;
-            break;
-          }
+        // search by name logic
+        //-------------------------------------------------------
+        if (name) {
+            let names = name.split(' ');
+            for (let key in names) {
+                names[key] = new RegExp(names[key], 'i');
+            }
+            //finds the stylists based on search query
+            returnedStylists = await Stylist.find({
+                $or: [
+                    { firstName: { $in: names } },
+                    { lastName: { $in: names } },
+                ],
+            });
+        } else {
+            returnedStylists = await Stylist.find();
         }
-        return hasServices;
-      });
-    }
 
-    res.json({
-      success: true,
-      returnedStylists,
-    });
-  } catch (err) {
-    res.status(400).json({ msg: err });
-  }
+        //search by services logic
+        //-------------------------------------------------------
+        if (services) {
+            returnedStylists = returnedStylists.filter(stylist => {
+                let hasServices = true;
+                for (let service of services.split(' ')) {
+                    if (stylist.services.indexOf(service) === -1) {
+                        hasServices = false;
+                        break;
+                    }
+                }
+                return hasServices;
+            });
+        }
+
+        res.json({
+            success: true,
+            returnedStylists,
+        });
+    } catch (err) {
+        res.status(400).json({ msg: err });
+    }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,87 +1,91 @@
-import mongoose from "mongoose";
-import User from "../models/userModel.js";
-import ErrorResponse from "../utils/errorResponse.js";
+import mongoose from 'mongoose';
+import User from '../models/userModel.js';
+import ErrorResponse from '../utils/errorResponse.js';
 
 //@desc          Allow User to create an account
 //@route         POST /users/register
 //@access        Public
 export const createUser = async (req, res, next) => {
-  const user = new User(req.body);
-  try {
-    const newUser = await user.save();
-    //create token
-    const token = user.getSignedJwtToken();
+    const user = new User(req.body);
+    try {
+        const newUser = await user.save();
+        //create token
+        const token = user.getSignedJwtToken();
 
-    res.json({ sucess: true, token, newUser });
-  } catch (err) {
-    next(err);
-  }
+        res.json({ sucess: true, token, newUser });
+    } catch (err) {
+        next(err);
+    }
 };
 
 //@desc          Update user based on userId
 //@route         PUT /users/:id
 //@access        Private
 export const updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return next(new ErrorResponse("Cannot Find Resource", 404));
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return next(new ErrorResponse('Cannot Find Resource', 404));
+        }
+        if (!req.user || req.user.id !== user.id) {
+            return next(new ErrorResponse('Unauthorized', 401));
+        }
+        ['address', 'firstName', 'lastName', 'photo', 'number'].forEach(
+            prop => {
+                if (req.body[prop] && req.body[prop] !== user[prop]) {
+                    user[prop] = req.body[prop];
+                }
+            }
+        );
+        await user.save();
+        res.status(200).json({
+            sucess: true,
+            user,
+        });
+    } catch (err) {
+        return next(err);
     }
-    if (!req.user || req.user.id !== user.id) {
-      return next(new ErrorResponse("Unauthorized", 401));
-    }
-    ["address", "firstName", "lastName", "photo", "number"].forEach((prop) => {
-      if (req.body[prop] && req.body[prop] !== user[prop]) {
-        user[prop] = req.body[prop];
-      }
-    });
-    await user.save();
-    res.status(200).json({
-      sucess: true,
-      user,
-    });
-  } catch (err) {
-    return next(err);
-  }
 };
 
 //@desc          Get all users and their information
 //@route         GET /users/
 //@access        Private?
 export const getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        next(err);
+    }
 };
 
 //@desc          Allow user to login by matching email and password
 //@route         POST /users/login
 //@access        Public
 export const userLogin = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (!email || !password) {
-    return next(new ErrorResponse("Please provide an email and password", 400));
-  }
-  try {
-    const currUser = await User.findOne({ email }).select("+password");
-    if (!currUser) {
-      return next(new ErrorResponse("Invalid Credentials", 401));
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !password) {
+        return next(
+            new ErrorResponse('Please provide an email and password', 400)
+        );
     }
-    const isMatch = await currUser.matchPassword(password);
-    if (!isMatch) {
-      return next(new ErrorResponse("Invalid Credentials", 401));
+    try {
+        const currUser = await User.findOne({ email }).select('+password');
+        if (!currUser) {
+            return next(new ErrorResponse('Invalid Credentials', 401));
+        }
+        const isMatch = await currUser.matchPassword(password);
+        if (!isMatch) {
+            return next(new ErrorResponse('Invalid Credentials', 401));
+        }
+        const token = currUser.getSignedJwtToken();
+        currUser.lastLogin = Date.now();
+        currUser.save();
+        res.status(200).json({ success: true, token, user: currUser });
+    } catch (err) {
+        next(err);
     }
-    const token = currUser.getSignedJwtToken();
-    currUser.lastLogin = Date.now();
-    currUser.save();
-    res.status(200).json({ success: true, token, user: currUser });
-  } catch (err) {
-    next(err);
-  }
 };
 
 //@desc          Allow User to change password
@@ -96,25 +100,43 @@ should take in:
    }
 */
 export const changePassword = async (req, res, next) => {
-  try {
-    const currUser = await User.findById(req.params.userId).select("+password");
-    if (!req.user || req.user.id !== currUser.id) {
-      return next(new ErrorResponse("Unauthorized", 401));
+    try {
+        const currUser = await User.findById(req.params.userId).select(
+            '+password'
+        );
+        if (!req.user || req.user.id !== currUser.id) {
+            return next(new ErrorResponse('Unauthorized', 401));
+        }
+        const isMatch = await currUser.matchPassword(req.body.password);
+        if (!isMatch) {
+            return next(new ErrorResponse('Invalid password', 400));
+        }
+        if (req.body.newPassword !== req.body.newPasswordConf) {
+            return next(new ErrorResponse('new passwords do not match', 400));
+        }
+        currUser.password = req.body.newPassword;
+        await currUser.save();
+        res.status(200).json({
+            success: true,
+            msg: 'Password Updated',
+        });
+    } catch (err) {
+        next(err);
     }
-    const isMatch = await currUser.matchPassword(req.body.password);
-    if (!isMatch) {
-      return next(new ErrorResponse("Invalid password", 400));
+};
+
+//@desc          Get current logged in user
+//@route         GET /users/me
+//@access        Private
+
+export const getMe = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    } catch (err) {
+        next(err);
     }
-    if (req.body.newPassword !== req.body.newPasswordConf) {
-      return next(new ErrorResponse("new passwords do not match", 400));
-    }
-    currUser.password = req.body.newPassword;
-    await currUser.save();
-    res.status(200).json({
-      success: true,
-      msg: "Password Updated",
-    });
-  } catch (err) {
-    next(err);
-  }
 };
