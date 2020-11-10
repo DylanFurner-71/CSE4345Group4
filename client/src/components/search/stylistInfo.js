b8d0c78873e3e1719fce8badb9652704887d5875
jameshhe <jameshuang@smu.edu>
2020-11-09 23:04:48 -0600

    added loading screen

--- client/src/components/search/stylistInfo.js
+++ client/src/components/search/stylistInfo.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import Rating from "../rating/rating";
 
 // Information span for each stylist
 const StylistInfo = ({stylist}) => {
-
-
     return (
-        <div className="container-fluid w-75">
-            {console.log(stylist)}
+        <div className="container-fluid w-75 my-2">
+            <hr/>
             <div className="row">
                 <div className="col-3 p-0">
                     <img src={stylist.photo} className="productImage" alt="Product"/>
                 </div>
-                <div className="col-7">
+                <div className="col-6">
                     <h4>{stylist.firstName} {stylist.lastName}</h4>
                     <h5><span className="badge badge-primary">{stylist.businessName}</span></h5>
                 </div>
-                <div className="col-2">
-                    <p>Reviews</p>
+                <div className="col-3">
+                    <Rating rating={stylist.average}/>
+                    <p className="text-muted d-inline ml-1">({stylist.reviews.length})</p>
                 </div>
             </div>
+            <hr/>
         </div>
     );
 };
--- client/src/components/search/stylistsList.js
+++ client/src/components/search/stylistsList.js
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom'
 import axios from 'axios';
 import StylistInfo from "./stylistInfo";
+import Loading from "../loading";
 
 const StylistsList = () => {
     const stylistName = useParams()
     const URL = 'http://localhost:8000/stylists/search'
     const [stylists, setStylists] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const getStylists = async () => {
@@ -17,6 +19,7 @@ const StylistsList = () => {
             })
                 .then(res => {
                     setStylists(res.data.returnedStylists)
+                    setIsLoading(false)
                     console.log('Stylists data fetched')
                 })
                 .catch(err => console.log(err))
@@ -26,15 +29,19 @@ const StylistsList = () => {
     }, [])
 
     return (
-        <div>
-            <ul>
-                {stylists.map((stylist, index) => (
-                    <StylistInfo
-                        key={index}
-                        stylist={stylist}
-                    />
-                ))}
-            </ul>
+        <div className="h-100">
+            {
+                isLoading ? <Loading/> :
+                <ul>
+                    {stylists.map((stylist, index) => (
+                        <StylistInfo
+                            key={index}
+                            stylist={stylist}
+                        />
+                    ))}
+                </ul>
+            }
+
         </div>
     );
 };
