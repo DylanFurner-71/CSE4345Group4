c3d6c02e6b3ad47989070e5cdd112b117f0b4512
Eli <elisego45@gmail.com>
2020-11-12 13:13:20 -0600

    Remove eli db

--- atlasConnect.js
+++ atlasConnect.js
@@ -1,12 +1,6 @@
 import mongoose from 'mongoose';
 
-const amTesting = false; // this is a bool to test on my mongo database
-// Can get rid of when ready to move into actual DB
-const testName = // again this is just a testing environment for me atm.
-    'mongodb+srv://elisego45:120060@segoviacluster0.zz1fg.mongodb.net/testHaircutBE?retryWrites=true&w=majority';
-
-export const ATLAS_URL = amTesting ? testName : process.env.MONGO_URI; // setting the uri to appropriate.
-// ---- can delete this line and up (except import obviously) when done...----------------
+export const ATLAS_URL = process.env.MONGO_URI; // setting the uri to appropriate.
 
 export const mongooseConnect = async () => {
     await mongoose
