8375b247d8a20468c058ea2aa6678127b5b487a9
Eli <elisego45@gmail.com>
2020-11-13 14:10:46 -0600

    Add map

--- client/src/components/search/map.js
+++ client/src/components/search/map.js
@@ -1,45 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactGoogleMapLoader from 'react-google-maps-loader';
 import GoogleMapReact from 'google-map-react';
 import { Icon } from '@iconify/react';
 import locationIcon from '@iconify/icons-mdi/map-marker';
 import './map.css';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const YOUR_API_KEY = 'AIzaSyCHjOs8sCJfI1C0HnzD5C1BahEx_XRQJvw';
 
-const LocationPin = ({ text }) => (
-    <div className='pin'>
+const useIsMount = () => {
+    const isMountRef = useRef(true);
+    useEffect(() => {
+        isMountRef.current = false;
+    }, []);
+    return isMountRef.current;
+};
+
+const LocationPin = ({ stylist, lng, lat, onClickLocation, clicked }) => (
+    <div onClick={e => onClickLocation({ lat, lng })} className='pin'>
         <Icon icon={locationIcon} className='pin-icon' />
-        <p className='pin-text'>{text}</p>
+        <div className={`pin-text ${clicked ? 'bg-white p-3' : null}`}>
+            <p className='h6'>{stylist.firstName + ' ' + stylist.lastName}</p>
+            {clicked && <hr />}
+            <p className='h6'>{stylist.businessName}</p>
+            {clicked && <small>{stylist.number}</small>}
+            {clicked && <small>{stylist.email}</small>}
+            {clicked && <hr />}
+            {clicked ? (
+                <>
+                    <p className='h6'>Services</p>
+                    <hr />
+                    <ul>
+                        {stylist.services.map((service, i) => {
+                            return <li key={i}>{service}</li>;
+                        })}
+                    </ul>
+                    <hr />
+                    <Link
+                        to={`/stylist/stylistId=${stylist._id.toString()}`}
+                        className='btn btn-block btn-primary'
+                    >
+                        More info
+                    </Link>
+                </>
+            ) : null}
+        </div>
     </div>
 );
 
-const handleClick = e => {};
-
 export const Map = ({ location, stylists }) => {
-    const [center, setCenter] = useState('');
-    const [zoom, setZoom] = useState(13);
+    const [center, setCenter] = useState([]);
+    const [zoom, setZoom] = useState(10);
+    const [target, setTarget] = useState({});
+
+    const handleClick = (location, stylist) => {
+        console.log(location);
+        setCenter([location.lat, location.lng]);
+        setTarget(stylist);
+    };
+
+    const isMount = useIsMount();
+
     return (
         <div className='map'>
-            <h2 className='map-h2'>Come Visit Us At Our Campus</h2>
-
             <div className='google-map'>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: YOUR_API_KEY }}
-                    defaultCenter={center ? center : location}
-                    defaultZoom={zoom}
+                    center={center.length > 0 ? center : location}
+                    zoom={zoom}
+                    onDrag={e => setTarget({})}
                 >
-                    {stylists.map(stylist => {
+                    {stylists.map((stylist, i) => {
                         return (
-                            <Link>
-                                <LocationPin
-                                    lat={stylist.location.coordinates[1]}
-                                    lng={stylist.location.coordinates[0]}
-                                    text={stylist.address}
-                                    onClick={e => handleClick(stylist)}
-                                />
-                            </Link>
+                            <LocationPin
+                                key={i}
+                                lat={stylist.location.coordinates[1]}
+                                lng={stylist.location.coordinates[0]}
+                                stylist={stylist}
+                                clicked={target && target._id == stylist._id}
+                                onClickLocation={location =>
+                                    handleClick(location, stylist)
+                                }
+                            />
                         );
                     })}
                 </GoogleMapReact>
